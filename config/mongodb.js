40a7c239a73c81e7ff08a74113c93cd0a0418e6d
Rafael Nery Machado <rafaelnerymachado@gmail.com>
2020-04-12 01:48:04 -0300

    Consertando mensagem de erro

--- config/mongodb.js
+++ config/mongodb.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.mongodb, { useNewUrlParser: true })
     .catch(e => {
         const msg = 'ERRO! Não foi possível conectar com o MongoDB!'
-        console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
+        console.log(msg)
     })
