e9f4617341aae7c93957e8074c39040dbd8971b8
Rafael Nery Machado <rafaelnerymachado@gmail.com>
2020-04-12 00:22:10 -0300

    Testando conexão com db

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,10 @@
 // Update with your config settings.
 module.exports = {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: true
+    },
     pool: {
       min: 2,
       max: 10
